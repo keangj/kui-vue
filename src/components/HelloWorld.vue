6ceb7755821345514bc6c0b057f43b324140dfa2
jay <keangjay@gmail.com>
2021-01-06 17:10:38 +0800

    使用TS，并添加vue-router

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <router-link to="/">home</router-link>
+  <router-link to="/about">about</router-link>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import { createRouter, createWebHashHistory }  from 'vue-router'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
