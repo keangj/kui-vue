0119c2ce78d0125a58626792c115ede083c40419
jay <keangjay@gmail.com>
2021-01-15 16:19:31 +0800

    feat(switch): 添加动画

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -34,8 +34,12 @@ export default {
     width: $h2;
     background:white;
     border-radius: $h2 / 2;
+    transition: left 250ms;
   } 
   button.checked > span {
     left: calc(100% - #{$h2} - 2px);
+  }
+  button:focus {
+    outline: none;
   }
 </style>
