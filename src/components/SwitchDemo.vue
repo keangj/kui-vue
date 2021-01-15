f1bc308f62775d5ad5cc545e9fe2918eb1a866d7
jay <keangjay@gmail.com>
2021-01-15 17:17:41 +0800

    feat(switch): 添加外部值和事件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,13 +1,18 @@
 <template>
-  <Switch />
+  <Switch :value="switchValue" @update:value="switchValue = $event"/>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue';
 import Switch from '../lib/Switch.vue';
 
 export default {
   components: {
     Switch
+  },
+  setup () {
+    const switchValue = ref(false);
+    return { switchValue }
   }
 }
 </script>
