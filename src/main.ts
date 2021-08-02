87b0b4e9d133e28060a56457fb688970210fa096
jay <keangjay@gmail.com>
2021-08-02 11:28:05 +0800

    feat(): 引入 github-markdown-css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { router } from './router';
 import './index.scss'
 import App from './App.vue'
+import 'github-markdown-css'
 
 const app = createApp(App)
 app.use(router)
